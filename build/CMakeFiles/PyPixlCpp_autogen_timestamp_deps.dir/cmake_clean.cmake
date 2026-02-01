cc3908de769a515332eb325c8297a13a7f95d2ff
maskedsyntax <aftaab@aftaab.xyz>
2026-02-01 20:19:47 +0530

    add build/ to .gitignore and stop tracking build artifacts

--- build/CMakeFiles/PyPixlCpp_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PyPixlCpp_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PyPixlCpp_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
